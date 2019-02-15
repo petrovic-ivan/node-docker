e66bd0405fe700211edca669ba0b7b02dc1e773f
Petrovic, Ivan <ivan.petrovic@moodys.com>
2019-02-15 14:56:49 -0800

    Added redis

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
+const client = redis.createClient();
+
+client.set('visits', 0);
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    client.get('visits', (err, visits) => {
+        res.send(`Number of visits is ${visits}.`);
+        client.set('visits', parseInt(visits) + 1);
+    });
+
 });
 
 app.listen(3000, () => {
